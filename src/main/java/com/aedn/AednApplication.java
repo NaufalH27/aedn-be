abaa921dd4573f77c7085630b8632a5ef317615b
NaufalH27 <naufalhmuzakki@gmail.com>
2026-03-26 15:12:58 +0700

    first commit

---
+++ src/main/java/com/aedn/AednApplication.java
@@ -0,0 +1,13 @@
+package com.aedn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AednApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AednApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aedn/AednApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aedn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AednApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
