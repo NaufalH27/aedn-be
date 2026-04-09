5d302e185f47e55889c3f4b36ede1d0bfd993734
NaufalH27 <naufalhmuzakki@gmail.com>
2026-04-09 08:13:54 +0700

    update example

--- src/main/java/com/aedn/controller/ExampleController.java
+++ src/main/java/com/aedn/controller/ExampleController.java
@@ -1,24 +1,30 @@
 package com.aedn.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aedn.dto.ExampleDto;
+import com.aedn.common.ApiResponse;
 import com.aedn.service.ExampleService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class ExampleController {
+
     private final ExampleService exampleService;
 
-    @Autowired
-    public ExampleController(ExampleService exampleServ) {
-        this.exampleService = exampleServ;
+    @GetMapping("/example")
+    public ResponseEntity<ApiResponse<ExampleDto>> getExample() {
+        return ResponseEntity.ok(ApiResponse.success("Get Example Success", exampleService.getExample()));
     }
 
-    @GetMapping("/example")
-    public ResponseEntity<ExampleDto> index() {
-        return ResponseEntity.ok(exampleService.getExample());
+    @PostMapping("/example")
+    public ResponseEntity<ApiResponse<ExampleDto>> postExample(@RequestBody ExampleDto dto) {
+        return ResponseEntity.ok(ApiResponse.success("Post Example Success", exampleService.nothing(dto)));
     }
 }
--- src/main/java/com/aedn/service/ExampleService.java
+++ src/main/java/com/aedn/service/ExampleService.java
@@ -3,27 +3,27 @@ package com.aedn.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aedn.dto.ExampleDto;
 import com.aedn.entity.Example;
 import com.aedn.repository.ExampleRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ExampleService {
     
     private final ExampleRepository exampleRepository;
 
-    @Autowired
-    public ExampleService(ExampleRepository exampleRepo) {
-        this.exampleRepository = exampleRepo;
-    }
-
     public ExampleDto getExample() {
         Optional<Example> entity = exampleRepository.findFirstBy();
-
         return entity.map(ExampleDto::fromEntity).orElse(null);
+    }
+
+    public ExampleDto nothing(ExampleDto dto) {
+        return dto;
     }
 
 
